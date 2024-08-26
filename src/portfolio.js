7f09c2e37d46ca8fbdcf926cb3e2d67a99bbc9f1
saikrishna <msaikrishna2603@gmail.com>
2024-08-26 18:49:54 -0400

    Changes for portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,10 +33,8 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
   linkedin: "https://www.linkedin.com/in/saadpasta/",
   gmail: "babitha@careerwenmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -368,9 +366,9 @@ const resumeSection = {
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+    "Just want to say hi? My Inbox is open for all.",
+  number: "+1 (845)-546-5289",
+  email_address: "babitha@careerwebmail.com"
 };
 
 // Twitter Section
@@ -380,7 +378,7 @@ const twitterDetails = {
   display: true // Set true to display this section, defaults to false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
